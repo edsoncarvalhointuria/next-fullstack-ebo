4ded42c7f1c67d6667fb0a8a0459e2df36d6507e
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2026-08-08 22:25:36 -0300

    atualizando logica de transacao

--- src/components/pages/transacoes/WrapperForm.tsx
+++ src/components/pages/transacoes/WrapperForm.tsx
@@ -1,13 +1,13 @@
 "use server";
 
-import { getItens } from "@/actions/handlerItens";
+import { getItemAtivo } from "@/actions/handlerItens";
 import FormTransacoes from "./FormTransacoes";
 
 export default async function WrapperForm({ link }: { link: string }) {
     const [ingressosData, cargosData, congregacoesData] = await Promise.all([
-        getItens("dimingresso"),
-        getItens("dimcargo"),
-        getItens("dimcongregacao"),
+        getItemAtivo("dimingresso"),
+        getItemAtivo("dimcargo"),
+        getItemAtivo("dimcongregacao"),
     ]);
     const ingressos = ingressosData.data!;
     const cargos = cargosData.data!;

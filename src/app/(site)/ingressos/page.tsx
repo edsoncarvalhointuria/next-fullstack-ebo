6ecb871dde1803f28f5a0d357f4cff1874a391e6
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2026-08-12 17:38:05 -0300

    removendo whats

--- src/app/(site)/ingressos/page.tsx
+++ src/app/(site)/ingressos/page.tsx
@@ -30,7 +30,7 @@ export default async function Ingressos() {
                     ))}
             </section>
 
-            <BotaoWhatsapp />
+            {/* <BotaoWhatsapp /> */}
         </MotionMain>
     );
 }
